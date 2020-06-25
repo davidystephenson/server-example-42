8d2728ca2fd148eae841dadcf113c2e3944a2ed4
David Y. Stephenson <david@davidystephenson.com>
2020-06-25 14:34:07 +0200

    Register endpoints

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+function getTest (request, response) {
+  response.send('test!')
+}
+
+app.get(
+  '/test',
+  getTest
+)
+
+const getHomepage = (request, response) => {
+  response.send(`
+    <html>
+      <head>
+        <title>Homepage</title>
+      </head>
+      <body>
+        <h1>Welcome</h1>
+
+        <marquee>to our fun webpage!!!!!!!!</marquee>
+      </body>
+    </html>
+  `)
+}
+
+app.get(
+  '/',
+  getHomepage
+)
+
+app.listen(
+  port,
+  () => {
+    console.log(`Server listening :${port}`)
+  }
+)
